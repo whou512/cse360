c75e41e18cf0a1d04bb9b8b60818bb6669c431aa
hwh <hwh@localhost>
2020-10-06 04:22:41 +0800

    second version

--- cse360/src/cse360assignment02/AddingMachine.java
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,40 @@
+//URL：https://github.com/whou512/cse360
+//ID of GitHub: whou512
 package cse360assignment02;
+
 
 public class AddingMachine {
   private int total;
+  private String trans;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    trans = "0";
   }
   
   public int getTotal () {
-    return 0;
+	  
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  trans += "+" +value;
+	  
   }
 
   public void subtract (int value) {
+	  total -= value;
+	  trans += "-" +value;
   }
 
   public String toString () {
-    return "";
+    return trans;
   }
 
   public void clear() {
+	  trans = "0";
   }
+ 
+  
 }
